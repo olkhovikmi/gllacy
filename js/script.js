8643ae0715798330223f17216a0c98d766462202
olkhovikmi <olkhovikmi3@gmail.com>
2020-04-07 02:04:36 +0300

    сдела слайдер на css

--- js/script.js
+++ js/script.js
@@ -1,27 +1,28 @@
 let openForm = document.querySelector('.search');
 
-openForm.onclick = function() {
+openForm.onclick = function () {
 	let elem = document.querySelector(".btn-search"); //находим блок div по его class, который передали в функцию
 	let state = elem.style.display; //смотрим, включен ли сейчас элемент
 	if (state == '') {
 		elem.style.display = 'block';
 	}//если включен, то выключаем
-	else { 
-		elem.style.display = ''; 
-} //иначе - включаем
-};
+	else {
+		elem.style.display = '';
+	} //иначе - включаем
+}
 
 let login = document.querySelector(".login");
 
 login.onclick = function () {
-let loginForm = document.querySelector(".login-form");
-let loginOn = loginForm.style.display;
-if (loginOn == "") {
-	loginForm.style.display = "block";
-} else {
-	loginForm.style.display = "";
+	let loginForm = document.querySelector(".login-form");
+	let loginOn = loginForm.style.display;
+	if (loginOn == "") {
+		loginForm.style.display = "block";
+	}	else {
+		loginForm.style.display = "";
+	}
 }
-};
+
 
 
 
